4c11edc91ac50a44f701200a50185bd29d76ac21
shivam Gupta <sgshivamgupta206.in>
2026-05-13 13:40:19 +0530

    backend fixes

--- Server/configs/multer.js
+++ Server/configs/multer.js
@@ -1,12 +1,22 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//   destination: "uploads/",
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   }
+// });
+
+// const upload = multer({ storage }); 
+
+// export default upload; 
+
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
 });
 
-const upload = multer({ storage }); 
-
-export default upload; 
+export default upload;
